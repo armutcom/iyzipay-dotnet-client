7e1f72a8cffbaad03955369fc64ede7d808c4ced
berkatil <berkatil@armut.com>
2019-09-10 15:16:59 +0300

    * LastFourDigits parameter is added to the Payment response

--- Iyzipay/Model/PaymentResource.cs
+++ Iyzipay/Model/PaymentResource.cs
@@ -22,6 +22,7 @@ namespace Armut.Iyzipay.Model
         public string CardToken { get; set; }
         public string CardUserKey { get; set; }
         public string BinNumber { get; set; }
+        public string LastFourDigits { get; set; }
         public string BasketId { get; set; }
         [JsonProperty(PropertyName = "itemTransactions")]
         public List<PaymentItem> PaymentItems { get; set; }
