9d28f2e28837616a490e6f5f1659816982e994c0
berkatil <berkatil@armut.com>
2019-09-11 10:40:02 +0300

    *retrieveTransactionDetail tests added

---
+++ Iyzipay.Tests/Functional/Builder/Request/RetrieveTransactionDetailRequestBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Armut.Iyzipay.Request.V2;
+
+namespace Armut.Iyzipay.Tests.Functional.Builder.Request
+{
+    public class RetrieveTransactionDetailRequestBuilder : BaseRequestBuilderV2<RetrieveTransactionDetailRequest>
+    {
+        public static RetrieveTransactionDetailRequestBuilder Create()
+        {
+            return new RetrieveTransactionDetailRequestBuilder();
+        }
+
+        public override RetrieveTransactionDetailRequest Build()
+        {
+            return new RetrieveTransactionDetailRequest
+            {
+                ConversationId = "123456789",
+                PaymentConversationId = "123456789"
+            };
+        }
+    }
+}
--- Iyzipay/Request/V2/RetrieveTransactionDetailRequest.cs
+++ Iyzipay/Request/V2/RetrieveTransactionDetailRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace Armut.Iyzipay.Request.V2
 {
-    public class RetrieveTransactionDetailRequest:BaseRequestV2
+    public class RetrieveTransactionDetailRequest : BaseRequestV2
     {
         public string PaymentConversationId { get; set; }
     }
