de0fb4e80ccafaf4a20b30281dc159ae832e838d
berkatil <atilberk98@gmail.com>
2019-11-22 20:36:10 +0300

    * PayWithIyzico client added (#22)
    * PayWithIyzico client added
    
    * version update

---
+++ src/Iyzipay/Model/PayWithIyzicoInitializeResource.cs
@@ -0,0 +1,13 @@
+﻿namespace Armut.Iyzipay.Model
+{
+    public class PayWithIyzicoInitializeResource : IyzipayResource
+    {
+        public string Token { get; set; }
+
+        public string CheckoutFormContent { get; set; }
+
+        public long? TokenExpireTime { get; set; }
+
+        public string PayWithIyzicoPageUrl { get; set; }
+    }
+}
---
+++ src/Iyzipay/Request/CreatePayWithIyzicoInitializeRequest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Armut.Iyzipay.Model;
+
+namespace Armut.Iyzipay.Request
+{
+    public class CreatePayWithIyzicoInitializeRequest : BaseRequest
+    {
+        public string Price { get; set; }
+
+        public string PaidPrice { get; set; }
+
+        public string BasketId { get; set; }
+
+        public string PaymentGroup { get; set; }
+
+        public string PaymentSource { get; set; }
+
+        public string Currency { get; set; }
+
+        public Buyer Buyer { get; set; }
+
+        public Address ShippingAddress { get; set; }
+
+        public Address BillingAddress { get; set; }
+
+        public List<BasketItem> BasketItems { get; set; }
+
+        public string CallbackUrl { get; set; }
+
+        public int? ForceThreeDS { get; set; }
+
+        public string CardUserKey { get; set; }
+
+        public string PosOrderId { get; set; }
+
+        public List<int> EnabledInstallments { get; set; }
+
+        public override string ToPKIRequestString()
+        {
+            return ToStringRequestBuilder.NewInstance()
+                .AppendSuper(base.ToPKIRequestString())
+                .AppendPrice("price", Price)
+                .Append("basketId", BasketId)
+                .Append("paymentGroup", PaymentGroup)
+                .Append("buyer", Buyer)
+                .Append("shippingAddress", ShippingAddress)
+                .Append("billingAddress", BillingAddress)
+                .AppendList("basketItems", BasketItems)
+                .Append("callbackUrl", CallbackUrl)
+                .Append("paymentSource", PaymentSource)
+                .Append("currency", Currency)
+                .Append("posOrderId", PosOrderId)
+                .AppendPrice("paidPrice", PaidPrice)
+                .Append("forceThreeDS", ForceThreeDS)
+                .Append("cardUserKey", CardUserKey)
+                .AppendList("enabledInstallments", EnabledInstallments)
+                .GetRequestString();
+        }
+    }
+}
